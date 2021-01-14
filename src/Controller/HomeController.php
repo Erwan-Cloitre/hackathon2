55167e26627d0947648fcc89e8f890951d5a644f
erwan cloitre <erwan.cloitre@gmail.com>
2021-01-14 17:15:12 +0100

    bouton

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -56,31 +56,16 @@ class HomeController extends AbstractController
             'api10' => $api10
         ]);
     }
+
     public function startGame()
     {
-        $apimanager = new ApiManager();
-        $api = $apimanager->selectAll();
-        $api2 = $apimanager->selectAll2();
-        $api3 = $apimanager->selectAll3();
-        $api4 = $apimanager->selectAll4();
-        $api5 = $apimanager->selectAll5();
-        $api6 = $apimanager->selectAll6();
-        $api7 = $apimanager->selectAll7();
-        $api8 = $apimanager->selectAll8();
-        $api9 = $apimanager->selectAll9();
-        $api10 = $apimanager->selectAll10();
-
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $apimanager = new ApiManager();
+            $api = $apimanager->selectOneHero($id);
+        }
         return $this->twig->render('Home/startGame.html.twig', [
-            'api' => $api,
-            'api2' => $api2,
-            'api3' => $api3,
-            'api4' => $api4,
-            'api5' => $api5,
-            'api6' => $api6,
-            'api7' => $api7,
-            'api8' => $api8,
-            'api9' => $api9,
-            'api10' => $api10
+            'api' => $api
         ]);
     }
 }
--- src/Model/ApiManager.php
+++ src/Model/ApiManager.php
@@ -6,6 +6,23 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class ApiManager
 {
+    public function selectOneHero(int $id)
+    {
+        $client = HttpClient::create();
+
+        $response = $client->request('GET', 'https://superheroapi.com/api/10214898537337411/' . $id);
+
+        $statusCode = $response->getStatusCode();
+
+        if ($statusCode === 200) {
+            $content = $response->getContent();
+
+            $content = $response->toArray();
+
+            return $content;
+        }
+    }
+
     public function selectAll()
     {
         $client = HttpClient::create();
